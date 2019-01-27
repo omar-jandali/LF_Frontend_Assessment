e276b8536f0a4eb00421bf0c3ca7069cb52cd0e5
omar jandali <omarjandali@omars-MacBook-Pro.local>
2019-01-26 17:31:16 -0800

    :fixed the issues with the carousel. It is now working properly. It now hides the voerflow and shifts the entire slider over for next and previous

--- app.js
+++ app.js
@@ -1,90 +1,33 @@
 console.log('this is a test');
-$('#content').html();
-var $content = $('#content');
+$('.slides').html();
+var slider = $('#slider');
+var $slides = $('.slides');
+var $slide = $('.slide');
 
-images = [
-    {
-        id:1,
-        image:'http://via.placeholder.com/500x500',
-        title:'Vega',
-        caption:'Vega is one of the leading global plant-based nutrition companies.'
-    },
-    {
-        id:2,
-        image:'http://via.placeholder.com/500x500',
-        title:'EightSleep',
-        caption:'Eight has developed the world\'s first smart mattress.'
-    },
-    {
-        id:3,
-        image:'http://via.placeholder.com/500x500',
-        title:'The Hundereds.com',
-        caption:'The Hundereds Shopify Plus eCommerse site features a 100% custom and seamless Wordpress integraion.'
-    },
-    {
-        id:4,
-        image:'http://via.placeholder.com/500x500',
-        title:'PRG Nation',
-        caption:'InnerSelf Technologies hopes to create4 a PRG Nation, built around their new bio-stimulant and monitoring system.'
-    },
-    {
-        id:5,
-        image:'http://via.placeholder.com/500x500',
-        title:'Qualo.com',
-        caption:'Immerseive, highly-branded, and customer-focused shopping experience. '
-    },
-]
-
-var counter = 0;
-// .find('.current').animate({"margin-left":"400px"})
-
-function renderWindow(){
-    var $first = '<div id="item" class="first" style="background-image: url(' + images[counter].image + ');"><p id="title"> ' + images[counter].title + ' </p><p id="caption"> ' + images[counter].caption + ' </p></div>';
-    var $second = '<div id="item" class="second" style="background-image: url(' + images[counter + 1].image + ')"><p id="title"> ' + images[counter + 1].title + ' </p><p id="caption"> ' + images[counter + 1].caption + ' </p></div>'
-    $content.append($second);
-    $content.append($first);
-}
+var counter = 1;
 
 function nextItem(){
-    $content.find('.first').fadeOut(() => {
+    $slides.animate({"margin-left":"-=512px"}, 1000, () => {
         counter++;
-        $content.find('.first').remove();
-        $content.find('.second').removeClass('second').addClass('first');
-        if(counter <= 3){
-            var $new = '<div id="item" class="second" style="background-image: url(' + images[counter + 1].image + ')"><p id="title"> ' + images[counter + 1].title + ' </p><p id="caption"> ' + images[counter + 1].caption + ' </p></div>'
-            $content.find('.first').before($new)
+        if(counter === $slide.length){
+            counter = 1;
+            $slides.css({"margin-left":"0"});
         }
-        if(counter > 3){
-            counter = 0;
-            var $new = '<div id="item" class="second" style="background-image: url(' + images[counter].image + ')"><p id="title"> ' + images[counter].title + ' </p><p id="caption"> ' + images[counter].caption + ' </p></div>'
-            $content.find('.first').before($new)
-        
-        }
-    });
-
+    });    
 }
 
 function previousItem(){
-    $content.find('.second').fadeOut(() => {
+    $slides.animate({"margin-left":"+=512px"}, 1000, () => {
         counter--;
-        $content.find('.second').remove();
-        $content.find('.first').removeClass('first').addClass('second');
-        if(counter >= 0){
-            var $new = '<div id="item" class="first" style="background-image: url(' + images[counter].image + ')"><p id="title"> ' + images[counter].title + ' </p><p id="caption"> ' + images[counter].caption + ' </p></div>'
-            $content.append($new)
+        if(counter < 1){
+            counter = 5;
+            $slides.css({"margin-left":"-2048px"});
         }
-        if(counter < 0){
-            counter = 3;
-            var $new = '<div id="item" class="first" style="background-image: url(' + images[counter + 1].image + ')"><p id="title"> ' + images[counter + 1].title + ' </p><p id="caption"> ' + images[counter + 1].caption + ' </p></div>'
-            $content.append($new)
-        }
-    });
-
+    })
 }
 
 function animate(){
 
-    renderWindow();
     setInterval(() => {
         nextItem()
     }, 5000);
